e3372c1aa65259b8d0552f14fffb4cc4ac5ee726
margulis <wfptny@gmail.com>
2025-01-18 14:16:51 +0100

    wow

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,15 @@ repositories {
     mavenCentral()
 }
 
+var h2Version="2.3.232"
+var slf4jVersion ="2.0.16"
+var logbackVersion= "1.4.12"
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.h2database:h2:$h2Version")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation ("ch.qos.logback:logback-classic:$logbackVersion")
 }
 
 tasks.test {
